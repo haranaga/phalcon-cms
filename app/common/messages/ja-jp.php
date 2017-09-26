1eb273ae22cb7593da16d1dd0572060e01824fbd
Munekazu Haranaga <haranaga@puzzle-ring.jp>
2017-09-26 17:12:03 +0900

    message ja symbolic link

--- app/common/messages/ja-jp.php
+++ app/common/messages/ja-jp.php
@@ -1,17 +1,1 @@
-<?php
-$messages =[
-    'lang' => 'ja-jp',
-    'test' => 'テスト！(jp)',
-    'menu' => 'メニュー',
-    'user' => 'ユーザー',
-    'user_login' => 'ログイン名',
-    'user_password' => 'パスワード',
-    'user_password_confirm' => 'パスワード確認',
-    'Already have an account?' => 'すでに登録済みの方',
-    'Sign In' => 'ログイン',
-    'is required' => '%name%を入力してください',
-    'is alphanumeric characters' => '%name%は半角英数字です',
-    'Please input' => '%name%を入力してください',
-    'not match' => '%a%と%b%が合っていません',
-    'Agree with terms of use?' => '<a href="%link%">ご利用規約</a>に同意する',
-];
+ja.php
